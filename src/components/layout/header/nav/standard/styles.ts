0ab94607fe1cb7e52d6c07989616e5d847bd9f59
Robert <majointa@gmail.com>
2023-04-10 14:42:43 +0200

    fix menu colours

--- src/components/layout/header/nav/standard/styles.ts
+++ src/components/layout/header/nav/standard/styles.ts
@@ -62,5 +62,6 @@ export const StyledLink = styled(Link)<{ isUniqueLink?: boolean }>`
   &:hover {
     opacity: 0.6;
     color: ${WEBSITE_BASE_COLOURS.CONTRAST_YELLOW}!important;
+    font-weight: bold;
   }
 `;
